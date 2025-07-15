0bc082a1314ed2310e485d43aed07e8aafcea561
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-07-16 00:06:53 +0300

    bugfix : add singleChildScrollview

--- lib/views/auth/change_password_page.dart
+++ lib/views/auth/change_password_page.dart
@@ -42,135 +42,140 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
     return Scaffold(
       appBar: AppBar(title: const Text("Change Password")),
       body: Padding(
-        padding: const EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(10.0),
         child: getNewPasswordWidgets(),
       ),
     );
   }
 
-  Column getNewPasswordWidgets() {
-    return Column(
-      children: [
-        Image.asset('assets/gifs/Forgot_password_1_transparent.gif'),
-        const SizedBox(height: 20),
-        GetBuilder<ChangePasswordController>(
-          init: changePController,
-          id: 'hideCurrent',
-          builder: (controller) => MyInputField(
-            hint: 'Current Password',
-            controller: currentController,
-            prefixIcon: Icons.password,
-            isObsecure: changePController.hideCurrent,
-            suffixWidget: IconButton(
-              onPressed: () {
-                changePController.flipHideCurrent();
-              },
-              icon: Icon(
-                changePController.hideCurrent
-                    ? Icons.remove_red_eye_outlined
-                    : Icons.remove_red_eye,
+  SingleChildScrollView getNewPasswordWidgets() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/gifs/Forgot_password_1_transparent.gif',
+          ),
+          const SizedBox(height: 20),
+          GetBuilder<ChangePasswordController>(
+            init: changePController,
+            id: 'hideCurrent',
+            builder: (controller) => MyInputField(
+              hint: 'Current Password',
+              controller: currentController,
+              prefixIcon: Icons.password,
+              isObsecure: changePController.hideCurrent,
+              suffixWidget: IconButton(
+                onPressed: () {
+                  changePController.flipHideCurrent();
+                },
+                icon: Icon(
+                  changePController.hideCurrent
+                      ? Icons.remove_red_eye_outlined
+                      : Icons.remove_red_eye,
+                ),
               ),
             ),
           ),
-        ),
-        GetBuilder<ChangePasswordController>(
-          init: changePController,
-          id: 'hideNew',
-          builder: (controller) => MyInputField(
-            hint: 'New Password',
-            controller: newController,
-            prefixIcon: Icons.password,
-            isObsecure: changePController.hideNew,
-            suffixWidget: IconButton(
-              onPressed: () {
-                changePController.flipHideNew();
-              },
-              icon: Icon(
-                changePController.hideNew
-                    ? Icons.remove_red_eye_outlined
-                    : Icons.remove_red_eye,
+          GetBuilder<ChangePasswordController>(
+            init: changePController,
+            id: 'hideNew',
+            builder: (controller) => MyInputField(
+              hint: 'New Password',
+              controller: newController,
+              prefixIcon: Icons.password,
+              isObsecure: changePController.hideNew,
+              suffixWidget: IconButton(
+                onPressed: () {
+                  changePController.flipHideNew();
+                },
+                icon: Icon(
+                  changePController.hideNew
+                      ? Icons.remove_red_eye_outlined
+                      : Icons.remove_red_eye,
+                ),
               ),
             ),
           ),
-        ),
-        GetBuilder<ChangePasswordController>(
-          init: changePController,
-          id: 'hideCNew',
-          builder: (controller) => MyInputField(
-            hint: 'Confirm Password',
-            controller: confirmController,
-            prefixIcon: Icons.password,
-            isObsecure: changePController.hideCNew,
-            suffixWidget: IconButton(
-              onPressed: () {
-                changePController.flipHideCNew();
-              },
-              icon: Icon(
-                changePController.hideCNew
-                    ? Icons.remove_red_eye_outlined
-                    : Icons.remove_red_eye,
+          GetBuilder<ChangePasswordController>(
+            init: changePController,
+            id: 'hideCNew',
+            builder: (controller) => MyInputField(
+              hint: 'Confirm Password',
+              controller: confirmController,
+              prefixIcon: Icons.password,
+              isObsecure: changePController.hideCNew,
+              suffixWidget: IconButton(
+                onPressed: () {
+                  changePController.flipHideCNew();
+                },
+                icon: Icon(
+                  changePController.hideCNew
+                      ? Icons.remove_red_eye_outlined
+                      : Icons.remove_red_eye,
+                ),
               ),
             ),
           ),
-        ),
-        GetBuilder<ChangePasswordController>(
-          init: changePController,
-          id: "updateButton",
-          builder: (controller) => MyButton(
-            title: changePController.updateLoading ? null : "Update",
-            onPressed: () async {
-              if (newController.text.isEmpty ||
-                  newController.text.trim() != confirmController.text.trim()) {
-                Get.showSnackbar(
-                  MySnackbar(
-                    success: false,
-                    title: "New Password",
-                    message: 'Passwords do not match',
-                  ),
+          GetBuilder<ChangePasswordController>(
+            init: changePController,
+            id: "updateButton",
+            builder: (controller) => MyButton(
+              title: changePController.updateLoading ? null : "Update",
+              onPressed: () async {
+                if (newController.text.isEmpty ||
+                    newController.text.trim() !=
+                        confirmController.text.trim()) {
+                  Get.showSnackbar(
+                    MySnackbar(
+                      success: false,
+                      title: "New Password",
+                      message: 'Passwords do not match',
+                    ),
+                  );
+                  return;
+                }
+                changePController.changeIsUpdateLoading(true);
+                bool result = await AuthApis.changePassword(
+                  currentPassword: currentController.text.trim(),
+                  newPassword: newController.text.trim(),
                 );
-                return;
-              }
-              changePController.changeIsUpdateLoading(true);
-              bool result = await AuthApis.changePassword(
-                currentPassword: currentController.text.trim(),
-                newPassword: newController.text.trim(),
-              );
-              changePController.changeIsUpdateLoading(false);
+                changePController.changeIsUpdateLoading(false);
 
-              if (result) {
-                TokenService.clearTokens();
-                changePController.clear();
+                if (result) {
+                  TokenService.clearTokens();
+                  changePController.clear();
 
-                await Api.box.write('rememberMe', false);
+                  await Api.box.write('rememberMe', false);
 
-                final ChatController chatController =
-                    Get.find<ChatController>();
+                  final ChatController chatController =
+                      Get.find<ChatController>();
 
-                final PropertyDetailsController pdController =
-                    Get.find<PropertyDetailsController>();
-                final PropertyController pController =
-                    Get.find<PropertyController>();
-                final MyPropertiesController myPController =
-                    Get.find<MyPropertiesController>();
-                chatController.clear();
-                pdController.clear();
-                pController.clear();
-                myPController.clear();
-                Get.offAllNamed('/login');
-              } else {
-                Get.showSnackbar(
-                  MySnackbar(
-                    success: false,
-                    title: 'Update Password',
-                    message:
-                        'Failed to update new password please try again later.',
-                  ),
-                );
-              }
-            },
+                  final PropertyDetailsController pdController =
+                      Get.find<PropertyDetailsController>();
+                  final PropertyController pController =
+                      Get.find<PropertyController>();
+                  final MyPropertiesController myPController =
+                      Get.find<MyPropertiesController>();
+                  chatController.clear();
+                  pdController.clear();
+                  pController.clear();
+                  myPController.clear();
+                  Get.offAllNamed('/login');
+                } else {
+                  Get.showSnackbar(
+                    MySnackbar(
+                      success: false,
+                      title: 'Update Password',
+                      message:
+                          'Failed to update new password please try again later.',
+                    ),
+                  );
+                }
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
