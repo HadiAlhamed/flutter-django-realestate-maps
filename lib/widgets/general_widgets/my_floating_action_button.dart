e2d8e3272fc5631c2d265fd743a4fc25aa7a9560
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-08-10 23:38:00 +0300

    feat : add simple animation for addProperty FAB

--- lib/widgets/general_widgets/my_floating_action_button.dart
+++ lib/widgets/general_widgets/my_floating_action_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:real_estate/controllers/main_controllers/account_page_controller.dart';
+import 'package:real_estate/textstyles/text_colors.dart';
 
 class MyFloatingActionButton extends StatelessWidget {
   const MyFloatingActionButton({super.key});
@@ -8,22 +9,53 @@ class MyFloatingActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<AccountPageController>(
-        id: "sellerMode",
-        builder: (controller) {
-          return FloatingActionButton(
-            backgroundColor: !controller.isSeller ? Colors.grey : null,
-            onPressed: !controller.isSeller
-                ? null
-                : () {
+      id: "sellerMode",
+      builder: (controller) {
+        final isSeller = controller.isSeller;
+
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          decoration: BoxDecoration(
+            // We wrap the FAB inside a container that animates color,
+            // but to keep shadow and shape like FAB, use Material with shape
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(15),
+            color: isSeller
+                ? Theme.of(context).brightness == Brightness.dark
+                    ? primaryColor
+                    : primaryColorInactive
+                : Colors.grey,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withAlpha((0.3 * 255).round()),
+                blurRadius: 6,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: FloatingActionButton(
+            backgroundColor:
+                Colors.transparent, // transparent, color handled by container
+            elevation: 0,
+            onPressed: isSeller
+                ? () {
                     Get.offNamed('/addPropertyPage', arguments: {
                       "isAdd": true,
                     });
-                  },
+                  }
+                : null,
             child: Icon(
               Icons.add,
-              color: !controller.isSeller ? Colors.grey[300] : null,
+              color: isSeller
+                  ? Theme.of(context).brightness == Brightness.dark
+                      ? Colors.black
+                      : Colors.white
+                  : Colors.grey[300],
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
